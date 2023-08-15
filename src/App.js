affe082b8e1377a480c265c4da48170a0ea3caf8
salindaGunarathna <salinda.eng@gmail.com>
2023-08-15 21:43:16 +0530

    react axios practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import axios  from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [DeviceName,setDeviceName]  = useState();
+  const [DeviceColor,setDeviceColor]  = useState();
+  const [DeviceCapacity,setDeviceCapacity]  = useState();
+
+  const url = "https://api.restful-api.dev/objects";
+
+  useEffect(()=>{
+    
+    const fetchData = async()=>{
+
+      try{
+        const  result = await axios.get(url);
+
+        console.log (result);
+        console.log ('working');
+
+
+      const needData = result.data[2];
+
+      setDeviceName(needData.name);
+      setDeviceColor(needData.data.color);
+      setDeviceCapacity(needData.data['capacity GB']);
+
+      }catch(error){
+        console.log(error);
+
+      }
+
+      
+    };
+
+    fetchData();
+
+  },[]);
+
+
+  return(
+
+    <div className="backGround">
+    <div className="device">{DeviceName}</div>
+    <div className="device-color">{DeviceColor}</div>
+    <div className="device-capaciry">{DeviceCapacity}</div>
+
     </div>
-  );
+
+
+
+  ) ;
 }
 
 export default App;
